aab7ebad007b4629304a65d2fc0e571c4048ba13
Marcq <Julien.Marcq@Exxeta.com>
2023-12-21 15:20:00 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.astro"],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'transparent': 'transparent',
+      'primary': '#c0cc19',
+      'secondary': '#4a5568',
+    },
+    container: {
+      padding: '1rem',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
