911c9f4505c8b73ef6e49af675371dabf4477daf
Marcq <Julien.Marcq@Exxeta.com>
2024-01-12 14:41:47 +0100

    Added contributor section

---
+++ src/accessibility.ts
@@ -0,0 +1,11 @@
+export const correo_start_page = `You can see the Correo start page. Two central Publish functionalities are located on the left-hand side. A topic with QOS can be specified in the upper square. The payload can also be entered in a separate window in this area. In the lower square on the left-hand side is the history of published messages.
+The right-hand side of the start page also consists of several squares. The top square on the right-hand side is the same height as the Publish square on the left.
+In this square, users can subscribe to a topic and also enter a QOS. In a list underneath, the user can see which topics are currently subscribed to.
+The bottom half of the right-hand side consists of two adjacent squares. The left-hand square contains the history of messages received. The right-hand side is a detailed view of a received view.`;
+
+export const correo_feature_link = "Read more about the Correo Features";
+export const correo_documentation_link = "Read more about Correo Functionality in our Github Wiki.";
+export const correo_contributing_link = "Read more about our Contributing Guidelines.";
+export const correo_plugin_link = "Read more about the Correo Plugin ecosystem and see what Plugins are avaibable.";
+export const correo_download_link = "Follow this link to download the Correo Desktop App.";
+export const correo_github_link = "Follow this link to see the Correo Github Repository.";
